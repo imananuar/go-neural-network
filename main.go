efe077b68622d9d7fe68ebed1c7b46d82a7cc6ba
imananuar <imananuar5367@gmail.com>
2024-10-17 22:40:57 +0800

    Subtract matrix & transpose matrix

--- main.go
+++ main.go
@@ -12,13 +12,24 @@ func main() {
 
 	matrix2 := [][] int {
 		{1, 2, 3},
-		{4, 5, 6},
+		{4, 5, -6},
 	}
-	
+
 	totalMatrix, err:= AddMatrix(matrix1, matrix2)
 	if err != nil {
 		fmt.Println("Error:", err)
 		return
 	}
-	PrintMatrix(totalMatrix, "Sum of Matrix")
+
+	tMatrix := TransposeMatrix(matrix1);
+
+	subtractMatrix, err:= SubtractMatrix(matrix1, matrix2)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	PrintMatrix("Sum of Matrix", totalMatrix)
+	PrintMatrix("Transpose Matrix", tMatrix)
+	PrintMatrix("Subtract Matrix", subtractMatrix)
+
 }
--- operation.go
+++ operation.go
@@ -21,6 +21,45 @@ func AddMatrix(matrix1, matrix2 [][]int) ([][]int, error) {
 	return sumMatrix, nil
 }
 
+// create subtract
+func SubtractMatrix(matrix1, matrix2 [][]int) ([][]int, error) {
+	if len(matrix1[0]) != len(matrix2[0]) || len(matrix1) != len(matrix2) {
+		return nil, fmt.Errorf("matrices must be in the same dimensions")
+	}
+	cols := len(matrix1[0])
+	rows := len(matrix1)
+
+	sumMatrix := CreateZeroMatrix(rows, cols)
+	fmt.Println(sumMatrix)
+	
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			sumMatrix[row][col] = matrix1[row][col] - matrix2[row][col]
+		}
+	}
+	return sumMatrix, nil
+}
+
+// create transpose
+func TransposeMatrix(matrix [][]int) [][]int {
+
+	rows := len(matrix[0])
+	cols := len(matrix)
+
+	tMatrix := CreateZeroMatrix(rows, cols)
+	for col := 0; col < cols; col++ {
+		for row := 0; row < rows; row++ {
+			tMatrix[row][col] = matrix[col][row]
+		}
+	}
+	
+	
+	return tMatrix
+}
+
+// create dot multiplication
+// create vector multiplication=
+
 func CreateZeroMatrix(rows, cols int) [][]int {
 	matrix := make([][]int, rows)
 	for i := range matrix {
@@ -29,7 +68,7 @@ func CreateZeroMatrix(rows, cols int) [][]int {
 	return matrix
 }
 
-func PrintMatrix(matrix [][]int, matrixName string) {
+func PrintMatrix(matrixName string, matrix [][]int) {
 	fmt.Println()
 	fmt.Println(matrixName, ": ")
 	for _, row := range matrix {
